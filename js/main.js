603cf9df8c562a2e6708828a7adecf13ce272f99
hoa22112000 <hoa.nd@haposoft.com>
2021-07-15 10:54:35 +0700

    issues 2

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
-
-// $(window).resize(function() {
-//     var width = $(window).width();
-//     if (width > 414){
-//         $(".collapse").addClass('show');
-//     }
-//   });
-
 $(".item").click(function (e) { 
     e.preventDefault();
     $(".item").removeClass("item-active");
