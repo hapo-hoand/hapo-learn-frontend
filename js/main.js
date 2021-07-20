2fef2a964d37ec600db4ecb4fad6ff4e1dbcfa1b
hoa22112000 <hoa.nd@haposoft.com>
2021-07-20 15:46:09 +0700

    issues - 5

--- js/main.js
+++ js/main.js
@@ -1,9 +1,27 @@
-$(".item").click(function (e) { 
+$(".item-menu").click(function (e) { 
     e.preventDefault();
-    $(".item").removeClass("item-active");
+    $(".item-menu").removeClass("item-active");
     $(this).addClass("item-active")
 });
 
 $(".btn-menu").click(function(){
     $(this).toggleClass("rotate");
+})
+
+$(document).on('ready', function() {
+    $('.autoplay').slick({
+        dots: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        speed: 1500,
+        responsive: [{
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+            },
+        }]
+    });
 })
