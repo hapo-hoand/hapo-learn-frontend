c1667ec195074193a1480dc284a945555d8799af
hoa22112000 <hoa.nd@haposoft.com>
2021-07-23 09:30:14 +0700

    fix name component

--- js/main.js
+++ js/main.js
@@ -1,49 +1,51 @@
-$(".item-menu").click(function (e) { 
-    e.preventDefault();
-    $(".item-menu").removeClass("item-active");
-    $(this).addClass("item-active")
+$(".item-menu").click(function (e) {
+  e.preventDefault();
+  $(".item-menu").removeClass("item-active");
+  $(this).addClass("item-active");
 });
 
-$(".btn-menu").click(function(){
-    $(this).toggleClass("rotate");
-})
+$(".btn-menu").click(function () {
+  $(this).toggleClass("rotate");
+});
 
-$(document).on('ready', function() {
-    $('.autoplay').slick({
-        dots: true,
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        speed: 1500,
-        responsive: [{
+$(document).on("ready", function () {
+  $(".autoplay").slick({
+    dots: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 1500,
+    responsive: [
+      {
         breakpoint: 1024,
         settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-            },
-        }]
-    });
-})
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
 
 $(function () {
-    $('.example-popover').popover({
-      container: 'body'
-    })
-})
+  $(".example-popover").popover({
+    container: "body",
+  });
+});
 
-$("#login").click(function(){
-    $("#exampleModal").modal("show")
-})
+$("#login").click(function () {
+  $("#modal-login").modal("show");
+});
 
-$(".close-modal").click(function(){
-    $("#exampleModal").modal("hide")
-})
+$(".close-modal").click(function () {
+  $("#modal-login").modal("hide");
+});
 
-$(".messenger").click(function(){
-    $(".wrap-mess").toggleClass("show")
-})
+$(".messenger").click(function () {
+  $(".wrap-mess").toggleClass("show");
+});
 
-$(".close-mess").click(function(){
-    $(".wrap-mess").toggleClass("show")
-})
+$(".close-mess").click(function () {
+  $(".wrap-mess").toggleClass("show");
+});
